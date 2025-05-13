624d44e80b70bb3fc0f64c9e6e80a2065e786808
Pix3;-Guy21 <bamlakadere@gmail.com>
2025-05-13 13:03:34 +0300

    modified:   App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native';
+import { StyleSheet, Text, View} from 'react-native';
 import { useState } from 'react';
 
 export default function App() {
@@ -17,7 +17,7 @@ export default function App() {
       </View>
       
       
-      {/* <StatusBar style="auto" /> */}
+      <StatusBar style="auto" />
     </View>
   );
 }
